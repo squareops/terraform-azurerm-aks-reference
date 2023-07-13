d9d1df5f072b2d1b32aa8465a524d6ca7984b4fe
anish-squareops <68534207+anish-squareops@users.noreply.github.com>
2023-07-13 18:47:25 +0530

    version 1.0.0 (#7)

---
+++ tfstate/main.tf
@@ -0,0 +1,14 @@
+locals {
+  region      = "East US"
+  environment = "prod"
+  name        = "skaf"
+}
+
+module "backend" {
+  source                  = "squareops/tfstate/azurerm"
+  resource_group_name     = local.name
+  storage_account_name    = local.name
+  storage_container_name  = "tfstate" # unique storage container name
+  resource_group_location = local.region
+  environment             = local.environment
+}
